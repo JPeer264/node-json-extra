ace6b7c4aa4e79bddf8229a6543096c2905360a1
JPeer264 <jan.oster94@gmail.com>
2016-12-01 22:06:42 +0100

    fixed toPath

--- test/toPath.spec.js
+++ test/toPath.spec.js
@@ -3,8 +3,8 @@
 var base   = require('../');
 var expect = require('chai').expect;
 
-describe('toPath.js', function () {
-    it('should create a path', function (done) {
+describe.only('toPath.js', function () {
+    it('should create a path', function () {
         var pathArray;
         var jsonObj = {
             one: {
@@ -17,21 +17,36 @@ describe('toPath.js', function () {
                         'three-one',
                         'three-two',
                         'three-three'
-                    ]
+                    ],
+                    'threetwo': {
+                        four: [
+                            'four-one',
+                            'four-two',
+                            'four-three'
+                        ]
+                    }
                 }
-            }
+            },
+            two: ['hallo']
         };
 
-        pathArray = base.toPath(jsonObj);
+        pathArray = base.toPath(jsonObj, '/');
 
-        console.log(pathArray);
-
-        done();
+        expect(pathArray[0]).to.equal('one');
+        expect(pathArray[1]).to.equal('one/two');
+        expect(pathArray[2]).to.equal('one/two/two-one');
+        expect(pathArray[3]).to.equal('one/two/two-two');
+        expect(pathArray[4]).to.equal('one/twotwo');
+        expect(pathArray[5]).to.equal('one/twotwo/three');
+        expect(pathArray[6]).to.equal('one/twotwo/three/three-one');
+        expect(pathArray[9]).to.equal('one/twotwo/threetwo');
     });
 
-    it('should not create a path', function (done) {
+    it('should not create a path', function () {
         var jsonString = '{test; "test"}';
+        var pathArray = base.toPath(jsonString);
 
-        expect(base.toPath(jsonString)).to.be.an('object');
+        expect(pathArray).to.be.an('array');
+        expect(pathArray.length).to.equal(0);
     });
 });
