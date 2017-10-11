1c7086c5da924d12f93642b5ad2272911b53f6b0
JPeer264 <jan.oster94@gmail.com>
2017-10-11 18:06:12 +0200

    Refactor: modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ]
+    extends: 'airbnb-base',
+    plugins: [
+        'import'
+    ],
+    rules: {
+        'import/prefer-default-export': 0
+    }
 };
