8344559617918df62b6ea1b82bca08b79556a647
JPeer264 <jan.oster94@gmail.com>
2020-03-04 14:25:42 +0900

    Chore: add newer eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,46 @@
 module.exports = {
-    extends: 'airbnb-base',
-    plugins: [
-        'import'
-    ],
-    rules: {
-        'import/prefer-default-export': 0
-    }
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base'
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-dupe-class-members': 'off',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['warn', {
+      allowTypedFunctionExpressions: true,
+      allowExpressions: true,
+    }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      ts: 'never',
+    }],
+  }
 };
